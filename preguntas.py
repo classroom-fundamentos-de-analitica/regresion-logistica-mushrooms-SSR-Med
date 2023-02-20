a1507459761c2c3a4a57c14848a47da9895704ab
SSR-MED <sasalazarr@unal.edu.co>
2023-02-19 21:29:19 -0500

    1.1

--- preguntas.py
+++ preguntas.py
@@ -71,7 +71,7 @@ def pregunta_01():
     df.drop("veil_type", axis=1, inplace=True)
 
     # Asigne la columna `type` a la variable `y`.
-    y = df["type"].values
+    y = df["type"]
 
     # Asigne una copia del dataframe `df` a la variable `X`.
     X = df.copy()
